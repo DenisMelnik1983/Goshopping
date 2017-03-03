93270009a8a2f080c8daa01c1c0bdbb43bf8ab9a
Denis Melnik <denismi@ukr.net>
2017-03-03 18:22:28 +0200

    add to mainActivity Toast for test add empty coment

--- app/src/main/java/ua/kiev/naparaplane/goshopping/activitys/MainActivity.java
+++ app/src/main/java/ua/kiev/naparaplane/goshopping/activitys/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        Toast.makeText(getApplicationContext(),"ddd", Toast.LENGTH_LONG).show();
+        //
     }
 }
